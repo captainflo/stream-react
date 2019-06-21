dcd31b1ff196a41006c72f75bdeff766d6726de4
florian lahitte <lahitte.florian@gmail.com>
2019-06-21 11:35:47 -0400

    Add bulk action creator for stream

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReacDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App.js';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware())
-    );
+const store = createStore(reducers,composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReacDOM.render(
     <Provider store={store}>
